01b60faf2ac6ac0741aa14fe57981701faca4a9a
hadacchi <master@hadacchi.com>
2025-09-06 20:37:50 +0900

    change run_internal attrs.

--- core.py
+++ core.py
@@ -18,7 +18,7 @@ logger = logging.getLogger(__name__)
 
 SUBCOMMANDS = ['devices', 'del', 'add', 'delmac', 'addmac']
 
-def run_internal(attrs, say, config):
+def run_internal(attrs, bot_client, message, config):
     '''
     @BOTNAME ap
         devices           登録済デバイスのMACアドレスのリストを取得
@@ -31,9 +31,11 @@ def run_internal(attrs, say, config):
     logger.info(f'ap runs: {attrs}')
     ap_config_file = config['config_file']
 
+    channel_id = message['channel']
+
     if attrs is None:
         logger.error('This plugin requires SUBCOMMAND.')
-        say(msgs.error())
+        bot_client.chat_postMessage(channel=channel_id, text=msgs.error())
         return False
 
     #say(msgs.confirm())
@@ -43,7 +45,7 @@ def run_internal(attrs, say, config):
 
     if subcommand not in SUBCOMMANDS:
         logger.error('Invalid subcommand: ' + subcommand)
-        say(msgs.error())
+        bot_client.chat_postMessage(channel=channel_id, text=msgs.error())
         return False
 
     if subcommand == 'devices':
@@ -51,9 +53,9 @@ def run_internal(attrs, say, config):
         ASsite = AirStationWebsite(ap_config_file)
         try:
             devices = ASsite.get_registered_devices()
-            say(str(devices))
+            bot_client.chat_postMessage(channel=channel_id, text=str(devices))
         except Exception as e:
-            say(str(e))
+            bot_client.chat_postMessage(channel=channel_id, text=str(e))
             logger.error(str(e))
         ASsite.exit()
     elif subcommand == 'delmac':
@@ -61,12 +63,12 @@ def run_internal(attrs, say, config):
         ASsite = AirStationWebsite(ap_config_file)
         try:
             if ASsite.del_mac_addr(attrs[1]):
-                say(msgs.finish())
+                bot_client.chat_postMessage(channel=channel_id, text=msgs.finish())
             else:
-                say(msgs.error())
+                bot_client.chat_postMessage(channel=channel_id, text=msgs.error())
                 logger.error('del_mac_addr returns False')
         except Exception as e:
-            say(str(e))
+            bot_client.chat_postMessage(channel=channel_id, text=str(e))
             logger.error(str(e))
         ASsite.exit()
     elif subcommand == 'addmac':
@@ -74,12 +76,12 @@ def run_internal(attrs, say, config):
         ASsite = AirStationWebsite(ap_config_file)
         try:
             if ASsite.add_mac_addr(attrs[1]):
-                say(msgs.finish())
+                bot_client.chat_postMessage(channel=channel_id, text=msgs.finish())
             else:
-                say(msgs.error())
+                bot_client.chat_postMessage(channel=channel_id, text=msgs.error())
                 logger.error('add_mac_addr returns False')
         except Exception as e:
-            say(str(e))
+            bot_client.chat_postMessage(channel=channel_id, text=str(e))
             logger.error(str(e))
         ASsite.exit()
     elif match.find('del') >= 0:
@@ -87,12 +89,12 @@ def run_internal(attrs, say, config):
         ASsite = AirStationWebsite(ap_config_file)
         try:
             if ASsite.del_device(' '.join(attrs[1:])):
-                say(msgs.finish())
+                bot_client.chat_postMessage(channel=channel_id, text=msgs.finish())
             else:
-                say(msgs.error())
+                bot_client.chat_postMessage(channel=channel_id, text=msgs.error())
                 logger.error('del_device returns False')
         except Exception as e:
-            say(str(e))
+            bot_client.chat_postMessage(channel=channel_id, text=str(e))
             logger.error(str(e))
         ASsite.exit()
     elif match.find('add') >= 0:
@@ -100,12 +102,12 @@ def run_internal(attrs, say, config):
         ASsite = AirStationWebsite(ap_config_file)
         try:
             if ASsite.add_device(' '.join(attrs[1:])):
-                say(msgs.finish())
+                bot_client.chat_postMessage(channel=channel_id, text=msgs.finish())
             else:
-                say(msgs.error())
+                bot_client.chat_postMessage(channel=channel_id, text=msgs.error())
                 logger.error('add_device returns False')
         except Exception as e:
-            say(str(e))
+            bot_client.chat_postMessage(channel=channel_id, text=str(e))
             logger.error(str(e))
         ASsite.exit()
     return True
